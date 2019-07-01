7b6eb8a4c6a5444017b9f7a13b9fddc47e9bca43
M H <ptcb795@gmail.com>
2019-07-01 22:44:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist= {xxx: "intro"};
 function updatePlaylist (playlist, artist,songtitle){
   
-  return Object.assign(playlist,{[artistname]:songtitle});
+  return Object.assign(playlist,{[artist]:songtitle});
 }
 function removeFromPlaylist(playlist, artist) {
   delete playlist.artist
