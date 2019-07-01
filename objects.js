1dd530a9f039d2d8b674d27bd00ea1f8c20d773e
M H <ptcb795@gmail.com>
2019-07-01 22:58:02 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ function updatePlaylist (playlist, artist,songtitle){
   return playlist
 }
 function removeFromPlaylist(playlist, artist) {
-  delete playlist.artist
+  delete playlist[artist]
 return playlist}
 
 
